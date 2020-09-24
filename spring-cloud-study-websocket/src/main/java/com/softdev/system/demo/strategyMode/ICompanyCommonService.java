fe9671abd30f7499b90c6d1a5e1e083e601a1440
liangbl <liangbaolong@biosan.cn>
2020-09-24 14:26:10 +0800

    websocket

---
+++ spring-cloud-study-websocket/src/main/java/com/softdev/system/demo/strategyMode/ICompanyCommonService.java
@@ -0,0 +1,14 @@
+package com.softdev.system.demo.strategyMode;
+
+/**
+ * @Description: TODO
+ * @ClassName: ICompanyCommonService
+ * @Author: liangbl
+ * @Date: 2020/9/24 13:27
+ */
+public interface ICompanyCommonService {
+
+    void say();
+
+    String getCompanyName();
+}
